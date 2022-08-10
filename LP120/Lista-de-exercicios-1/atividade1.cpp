27de20910ad6f7c4e0765c78ce08b9bda23940fb
LucasGFBatista <lukasbito@gmail.com>
2022-08-09 23:52:34 -0300

    Atividades LP120 Senai

---
+++ LP120/Lista-de-exercicios-1/atividade1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  float m, dm, cm, mm;
+
+  cout << "Digite um valor do metro: ";
+  cin >> m;
+
+  dm = m * 10;
+  cm = m * 100;
+  mm = m * 1000;
+
+  cout << "Decímetro: " << dm << endl;
+  cout << "Centímetro: " << cm << endl;
+  cout << "Milímetro: " << mm << endl;
+
+  return 0;
+}
