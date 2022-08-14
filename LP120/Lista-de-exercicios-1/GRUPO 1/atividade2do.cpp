e83e38d1a619485876ab3f2e6afe9f5ffb750909
LucasGFBatista <lukasbito@gmail.com>
2022-08-14 14:48:58 -0300

    Refazendo algumas atividades, tentando melhorar os codigos

--- LP120/Lista-de-exercicios-1/GRUPO 1/atividade2do.cpp
+++ LP120/Lista-de-exercicios-1/GRUPO 1/atividade2do.cpp
@@ -2,22 +2,73 @@
 2. Fazer um programa em C que imprime uma tabela com a tabuada de 1 a 9
 
 */
-
-
 #include <iostream>
 using namespace std;
 
 int main(){
-  int resultado, numero;
+  int resultado, numero, operacao, i = 0;
   
+  cout << "1 - Adicao" << endl;
+  cout << "2 - Subtracao" << endl;
+  cout << "3 - Multiplicacao" << endl;
+  cout << "4 - Divisao" << endl;
+  cout << "Escolha qual operacao da tabuada: ";
+  cin >> operacao;
+ 
+ switch (operacao){
+  case 1:
+    system("cls");
+    cout << "Adicao\n";
+    break;
+  case 2:
+    system("cls");
+    cout << "Subtracao\n";
+    break;
+  case 3:
+    system("cls");
+    cout << "Multiplicacao\n";
+    break;
+  case 4:
+    system("cls");
+    cout << "Divisao\n";
+    break;
+ }
+
   cout << "Digite o valor da tabuada: ";
   cin >> numero;
-  int i = 0; 
-  do{
-    resultado = numero * i;
-    cout << numero << "x" << i << "=" << resultado << endl;
+
+  if(operacao == 1){
+     do{
+    resultado = numero + i;
+    cout << numero << " + " << i << " = " << resultado << endl;
     i++;
     
-    } while(i < 10);
-  
+    } while(i <= 10);
+  }
+
+  if(operacao == 2){
+     do{
+    resultado = numero - i;
+    cout << numero << " - " << i << " = " << resultado << endl;
+    i++;
+    
+    } while(i <= 10);
+  }
+
+  if(operacao == 3){
+     do{
+    resultado = numero * i;
+    cout << numero << " * " << i << " = " << resultado << endl;
+    i++;
+    } while(i <= 10);
+  }
+
+  if(operacao == 4){
+     do{
+    resultado = numero + i;
+    cout << numero << " / " << i << " = " << resultado << endl;
+    i++;
+    } while(i <= 10);
+  }
+
 return 0;}
--- LP120/Lista-de-exercicios-1/GRUPO 1/atividade2for.cpp
+++ LP120/Lista-de-exercicios-1/GRUPO 1/atividade2for.cpp
@@ -6,13 +6,72 @@
 using namespace std;
 
 int main(){
-  int numero, resultado;
-  cout << "Digite o numero da tabuada desejada: ";
-  cin >> numero;
+  int resultado, numero, operacao, i;
 
-  for(int i = 0; i < 10;i++){
-    resultado = numero * i;
-    cout << numero << "x" << i << "=" << resultado << endl;
+  cout << "1 - Adicao" << endl;
+  cout << "2 - Subtracao" << endl;
+  cout << "3 - Multiplicacao" << endl;
+  cout << "4 - Divisao" << endl;
+  cout << "Escolha qual operacao da tabuada: ";
+  cin >> operacao;
+ 
+ switch (operacao){
+  case 1:
+    system("cls");
+    cout << "Adicao\n";
+    cout << "Digite o valor da tabuada: ";
+    cin >> numero;
+    break;
+  case 2:
+    system("cls");
+    cout << "Subtracao\n";
+    cout << "Digite o valor da tabuada: ";
+    cin >> numero;
+    break;
+  case 3:
+    system("cls");
+    cout << "Multiplicacao\n";
+    cout << "Digite o valor da tabuada: ";
+    cin >> numero;
+    break;
+  case 4:
+    system("cls");
+    cout << "Divisao\n";
+    cout << "Digite o valor da tabuada: ";
+    cin >> numero;
+    break;
+  default:
+    system("cls");
+    cout  << "Nao existe opcao. Por favor, escolher uma opercao da lista.\n";
+ }
+
+  
+
+  if(operacao == 1){
+    for (i = 0 ;i <= 10; i++ ){
+      resultado = numero + i;
+      cout << numero << " + " << i << " = " << resultado << endl;
+    }
   }
-  return 0;
+
+  if(operacao == 2){
+    for (i = 0; i <= 10; i++ ){
+      resultado = numero - i;
+      cout << numero << " - " << i << " = " << resultado << endl;
+    }
   }
+
+  if(operacao == 3){
+    for (i = 0; i <= 10; i++ ){
+      resultado = numero * i;
+      cout << numero << " * " << i << " = " << resultado << endl;
+    }
+  }
+
+  if(operacao == 4){
+    for (i = 0; i <= 10; i++ ){
+      resultado = numero / i;
+      cout << numero << " / " << i << " = " << resultado << endl;
+    }
+  }
+  return 0;}
