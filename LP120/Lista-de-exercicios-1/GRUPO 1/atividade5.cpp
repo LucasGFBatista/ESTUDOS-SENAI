51843daeaa01251896f250e04d09284556a58ac1
LucasGFBatista <lukasbito@gmail.com>
2022-08-13 22:53:53 -0300

    Atividades resolvidas em chamda com Juan Vinicus

---
+++ LP120/Lista-de-exercicios-1/GRUPO 1/atividade5.cpp
@@ -0,0 +1,44 @@
+#include <iostream> 
+#include <cmath>
+using namespace std;
+
+int main(){
+    float num1, num2, a, b, c, d, e, f;
+    
+    cout << "Digite o primeiro numero: ";
+    cin >> num1;
+    cout << "Digite o segundo numero: ";
+    cin >> num2;
+    
+    a = num1 + num2;
+    b = num1 * (num2 * num2);
+    c = num1 * num1;
+    d = sqrt((num1 * num1) +(num2 * num2));
+    e = sin(num1 - num2);
+    f = abs(num1);
+    
+    
+    
+    cout << "A soma é dos valores eh: " << a << endl;
+    cout << "O produto do primeiro numero pelo quadrado do segundo: " << b << endl;
+    cout << "O quadrado do primeiro: " << c << endl;
+    cout << "A raiz quadrada da soma dos quadrados: " << d << endl;
+    cout << "O seno da diferença do primeiro numero pelo segundo: " << e << endl;
+    cout << "O modulo do primeiro : " << f << endl;
+    
+    
+    return 0;
+}
+
+
+/*
+Fazer um programa em "C" que solicite 2 números e informe:
+
+a) A soma dos números;
+b) O produto do primeiro número pelo quadrado do segundo;
+c) O quadrado do primeiro número;
+d) A raiz quadrada da soma dos quadrados;
+e) O seno da diferença do primeiro número pelo segundo;
+f) O módulo do primeiro número. 
+
+*/
