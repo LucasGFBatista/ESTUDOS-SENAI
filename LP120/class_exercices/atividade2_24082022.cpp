ea673ede9165371d406e266086bc284f81865bd0
LucasGFBatista <lukasbito@gmail.com>
2022-08-26 21:01:02 -0300

    Apenas adicionei comentarios ao codigo

--- LP120/class_exercices/atividade2_24082022.cpp
+++ LP120/class_exercices/atividade2_24082022.cpp
@@ -2,6 +2,7 @@
 2 - FAÇA UM PROVA DE MATEMATICA PARA CRIANÇAS QUE ESTÃO APRENDENDO A SOMAR NUMEROS INTEIROS MENORES DO QUE 100. ESCOLHA NUMEROS ALEATORIOS ENTRE 1 E 100, E MOSTRE NA TELA A PERGUNTA: "QUAL A SOMA DE A + B?", ONDE "A" E "B" SÃO OS NUMEROS ALEATORIOS. PEÇA A RESPOSTSA. FAÇA CINCO(5) PERGUNTAS AO ALUNO, E MOSTRE PARA ELE AS PERGUNTAS E AS RESPOSTAS CORRETAS, ALEM DE QUANTAS VESES O ALUNO ACERTOU.
 */
 // https://github.com/LucasGFBatista
+// https://github.com/LucasGFBatista/ESTUDOS_SENAI/tree/master/LP120			onde estou subindo as atividades proposta na materia
 
 #include <iostream>
 #include <stdlib.h>
@@ -11,6 +12,8 @@ using namespace std;
 
 int main()
 {
+	setlocale(LC_ALL, "");
+
 	int numero1, numero2, resultado, resposta_aluno, acertos = 0;
 	char continuar;
 
@@ -18,15 +21,16 @@ int main()
 	{
 		for (int i = 0; i < 5; i++)
 		{
-			numero1 = rand() % 100 + 1;
-			numero2 = rand() % 100 + 1;
-			resultado = numero1 + numero2;
+			numero1 = rand() % 100 + 1;	   // gerar primeiro numero aleatorio
+			numero2 = rand() % 100 + 1;	   // gerar segundor numero aleatorio
+			resultado = numero1 + numero2; // somar os valores aleatorios
 
-			cout << "\n\nQual a Soma dos valores abaixo? \n";
+			cout << "\n------------------------------------\n";
+			cout << "\nQual a Soma dos valores abaixo? \n";
 			cout << numero1 << " + " << numero2 << " = ";
-			cin >> resposta_aluno;
+			cin >> resposta_aluno; // entrada de resposta do aluno
 
-			if (resultado != resposta_aluno)
+			if (resultado != resposta_aluno) // condição/comparação do valor dos numeros aleatorios e a resposta do aluno
 			{
 				cout << "\nSua resposta esta errada!"
 					 << "\nO resultado correto é: " << resultado;
@@ -37,10 +41,10 @@ int main()
 				cout << "Parabens! Resultado está correto!\n";
 			}
 		}
-		cout << "\nVocê acertou " << acertos << " de 5!";
-		cout << "\nDeseja continuar? (S/N) ";
+		cout << "\n\nVocê acertou " << acertos << " de 5!"; // qauntidade acertos do aluno
+		cout << "\n\nDeseja continuar? (S/N) ";				// caso ele queira dar continuidade em novos valores
 		cin >> continuar;
-		system("cls");
+		system("cls"); // limpa tela
 
 	} while (continuar == 's' || continuar == 'S');
 
