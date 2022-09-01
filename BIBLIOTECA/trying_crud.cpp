81fcbbbc3590586335695334d0f0b6954dc04603
LucasGFBatista <lukasbito@gmail.com>
2022-08-31 23:28:22 -0300

    Adicionado algumas funçoes

--- BIBLIOTECA/trying_crud.cpp
+++ BIBLIOTECA/trying_crud.cpp
@@ -1,30 +1,80 @@
+// RASCUNHO DE TRABALHO DE LOGICA DE PRGRAMÇÃO. CADASTRO DE BIBLIOTECA
+
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
+char opcao; // continuar cadastro (s/n)
 
-char opcao;
+// variaveis clientes
 const int lista_tamanho = 5;
 char nome[lista_tamanho][50];
 char email[lista_tamanho][50];
 int cpf[lista_tamanho];
 
+// variaveis livros
+char nome_livro[lista_tamanho][50];
+char nome_autor[lista_tamanho][50];
+int ano_publicacao[4];
+
+// chamar funções presentes no codigo
 void cadastro_cliente();
 void menu_principal();
+void cadastro_livro();
 
 int main(void)
 {
-    cadastro_cliente();
     menu_principal();
+    cadastro_cliente();
+    cadastro_livro();
+
     return 0;
 }
 
-void cadastro_cliente()
+void menu_principal() // função menu
+{
+    int opcao_menu;
+    bool ok;
+
+    do
+    {
+        cout << "*****************************\n";
+        cout << "1 - CADASTRO DE CLIENTE\n2 - CADASTRO DE LIVROS\n";
+        cout << "3 - PESQUISAR CLIENTE\n4 - PESQUISAR LIVRO\n";
+        cout << "*****************************\n";
+        cout << "DIGITE OPÇÃO DA TAREFA: ";
+        cin >> opcao_menu;
+
+        if (ok = !cin.fail())
+        {
+            switch (opcao_menu)
+            {
+            case 1:
+                system("cls");
+                cadastro_cliente();
+                break;
+            case 2:
+                system("cls");
+                cadastro_livro();
+            default:
+                system("cls");
+                cout << "\nOpção inválida\n";
+                ok = false;
+                break;
+            }
+        }
+    } while (!ok);
+}
+
+void cadastro_cliente() // função cadastro de clientes
 
 {
     static int linha;
-    
+
     do
     {
+        cout << "CADASTRO DE CLIENTES\n\n";
+
         cout << "\nDigite nome: ";
         cin >> nome[linha];
         cout << "\nDigite email: ";
@@ -36,36 +86,29 @@ void cadastro_cliente()
         cin >> opcao;
         linha++;
         system("cls");
-        
 
     } while (opcao == 's' || opcao == 'S');
 }
 
-void menu_principal(){
+void cadastro_livro() // função cadastro de livros
+{
+    static int linha;
 
-
-}
-
-
-void cadastro_livro(){
-        static int linha;
-    char 
     do
     {
+        cout << "CADASTRO DE LIVROS\n\n";
+
         cout << "\nDigite nome do livro: ";
-        cin >> nome[linha];
-        cout << "\nDigite email: ";
-        cin >> email[linha];
-        cout << "\nDigite CPF: ";
-        cin >> cpf[linha];
+        cin >> nome_livro[linha];
+        cout << "\nDigite nome do autor: ";
+        cin >> nome_autor[linha];
+        cout << "\nDigite ano de publicação: ";
+        cin >> ano_publicacao[linha];
 
         cout << "Deseja continuar? ";
         cin >> opcao;
         linha++;
         system("cls");
-        
 
     } while (opcao == 's' || opcao == 'S');
-}
-
 }
