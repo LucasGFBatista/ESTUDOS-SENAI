4b7a0677e51d355055271a332eddd9f6f7d80951
LucasGFBatista <lukasbito@gmail.com>
2022-08-31 22:31:06 -0300

    Criada a pasta do possivel Crud do trabalho final do semestre

--- BIBLIOTECA/trying_crud.cpp
+++ BIBLIOTECA/trying_crud.cpp
@@ -8,15 +8,18 @@ char nome[lista_tamanho][50];
 char email[lista_tamanho][50];
 int cpf[lista_tamanho];
 
-void cadastro();
+void cadastro_cliente();
+void menu_principal();
 
 int main(void)
 {
-    cadastro();
+    cadastro_cliente();
+    menu_principal();
     return 0;
 }
 
 void cadastro_cliente()
+
 {
     static int linha;
     
@@ -36,4 +39,33 @@ void cadastro_cliente()
         
 
     } while (opcao == 's' || opcao == 'S');
+}
+
+void menu_principal(){
+
+
+}
+
+
+void cadastro_livro(){
+        static int linha;
+    char 
+    do
+    {
+        cout << "\nDigite nome do livro: ";
+        cin >> nome[linha];
+        cout << "\nDigite email: ";
+        cin >> email[linha];
+        cout << "\nDigite CPF: ";
+        cin >> cpf[linha];
+
+        cout << "Deseja continuar? ";
+        cin >> opcao;
+        linha++;
+        system("cls");
+        
+
+    } while (opcao == 's' || opcao == 'S');
+}
+
 }
