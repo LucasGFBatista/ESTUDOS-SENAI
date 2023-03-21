c8b2754afc2ded37f19adac6001926eb17f7f5c8
Lucas Gonçalves <lucas.gf.batista@gmail.com>
2023-03-20 21:10:05 -0300

    Update Advogado.java

--- Semestre 2/DS1/Aulas/Aula-15.03.2023/src/Advogado.java
+++ Semestre 2/DS1/Aulas/Aula-15.03.2023/src/Advogado.java
@@ -1,34 +1,30 @@
 
 public class Advogado extends Funcionario {
-		private String oab;
+	private String oab;
 
-		public Advogado() {
-			super();
-		}
+	public Advogado() {
 
-		public Advogado(int id, String nome, int idade, Genero genero, String cpf, String rg, String oab , String matricula,
-				double salario, Setor setor , Endereco endereco) {
-			super(id, nome, idade, genero, cpf, rg, matricula, salario, setor, endereco);
-			this.oab = oab;
-		}
+	}
 
-		@Override
-		public String toString() {
-			return "\nAdvogado\n" + 
-					"\nNome: " + nome + 
-					"\nID: " + id + 
-					"\nMatricula: " + matricula + 
-					"\nCPF: " + cpf + 
-					"\nRG: " + rg + 
-					"\noab: " + oab +
-					"\nIdade: " + idade + 
-					"\nGenero: " + genero.valor + 
-					"\nSetor: " + Setor.ENGENHARIA.nomeSetor + 
-					"\nSalario: R$" + salario + 
-					"\n\nENDEREÇO\n" + endereco.toString();
-	
-	
-		
-		
-		
+	public Advogado(int id, String nome, int idade, Genero genero, String cpf, String rg, String oab, String matricula,
+			double salario, Setor setor, Endereco endereco) {
+		super(id, nome, idade, genero, cpf, rg, matricula, salario, setor, endereco);
+		this.oab = oab;
+	}
+
+	@Override
+	public String toString() {
+		return "\nAdvogado\n" + "\nNome: " + nome + "\nID: " + id + "\nMatricula: " + matricula + "\nCPF: " + cpf
+				+ "\nRG: " + rg + "\noab: " + oab + "\nIdade: " + idade + "\nGenero: " + genero.valor + "\nSetor: "
+				+ setor.JURIDICO + "\nSalario: R$" + salario + "\n\nENDEREÃ‡O\n" + endereco.toString();
+	}
+
+	public String getOab() {
+		return oab;
+	}
+
+	public void setOab(String oab) {
+		this.oab = oab;
+	}
+
 }
