6b2e98038b012fdc2bb65156807a2fe8c7e95b65
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-04-01 11:32:23 -0300

    aula 01/04/2023

---
+++ Semestre 2/DS1/Atividades/Atividade02/src/EstadoCivil.java
@@ -0,0 +1,20 @@
+
+public enum EstadoCivil {
+
+	SOLTEIRO("Solteiro(a)"), 
+	CASADO("Casado(a)"), 
+	SEPARADO("Separado(a)"),
+	DIVORCIADO("Divorciado(a)"),
+	VIUVO("Viuvo(a)");
+
+	protected String texto;
+
+	private EstadoCivil(String texto) {
+		this.texto = texto;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+}
