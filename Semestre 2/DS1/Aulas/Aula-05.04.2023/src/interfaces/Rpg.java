842f6d613441ac561c91787f749e0bfb9b376521
Lucas Gonçalves <lucas.gf.batista@gmail.com>
2023-04-05 21:11:28 -0300

    Add files via upload

---
+++ Semestre 2/DS1/Aulas/Aula-05.04.2023/src/interfaces/Rpg.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+public class Rpg implements Jogo {
+
+	@Override
+	public void jogar() {
+		System.out.println("Role o D6...");
+
+	}
+
+	@Override
+	public void fechar() {
+
+	}
+
+}
