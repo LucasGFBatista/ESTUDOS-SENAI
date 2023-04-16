38bb53a5373ef40d97c59a6fc2ed4bcfde05764b
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-04-16 19:18:14 -0300

    finalizado atividade

--- Semestre 2/DS1/Aulas/Aula-15.04.2023/Atividade01/CargoDeConfianca.java
+++ Semestre 2/DS1/Aulas/Aula-15.04.2023/Atividade01/CargoDeConfianca.java
@@ -1,6 +1,29 @@
 package Atividade01;
 
-public abstract class CargoDeConfianca extends Funcionario
-{
+import java.time.LocalDate;
+
+public abstract class CargoDeConfianca extends Funcionario {
+	
+	protected Bonificacao bonificacao;
+
+	// CONSTRUCTOR
+	public CargoDeConfianca() {
+		super();
+	}
+
+	public CargoDeConfianca(String nome, String cpf, Genero genero, double salarioBase, LocalDate dataAdmissao,
+			Bonificacao bonificacao) {
+		super(nome, cpf, genero, salarioBase, dataAdmissao);
+		this.bonificacao = bonificacao;
+	}
+
+	// GETTERS AND SETTERS
+	public Bonificacao getBonificacao() {
+		return bonificacao;
+	}
+
+	public void setBonificacao(Bonificacao bonificacao) {
+		this.bonificacao = bonificacao;
+	}
 
 }
