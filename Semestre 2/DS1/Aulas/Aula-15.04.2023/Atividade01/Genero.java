44dce0ab393292ec58db0d02eeeb24ff028dcbc8
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-04-17 19:50:57 -0300

    Lucas Só Atraso

--- Semestre 2/DS1/Aulas/Aula-15.04.2023/Atividade01/Genero.java
+++ Semestre 2/DS1/Aulas/Aula-15.04.2023/Atividade01/Genero.java
@@ -1,18 +1,5 @@
 package Atividade01;
 
 public enum Genero {
-	FEMININO("Feminino"), MASCULINO("Masculino");
-	
-	protected String texto;
 
-	private Genero(String texto) {
-		this.texto = texto;
-	}
-
-	public String getTexto() {
-		return texto;
-	}
-	
-	
-	
 }
