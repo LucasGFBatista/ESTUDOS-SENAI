c0d648eed17adf9842582585cd53abf6d567980c
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-04-30 21:44:45 -0300

    Iniciado loja

---
+++ Semestre-2/Programacao-web/Atividades/salao-de-beleza/index.php
@@ -0,0 +1,18 @@
+<form action="insert.php" method="post">
+    <div>
+        <label for="name">Digite o nome do usuário</label>
+        <input type="text" name="name" id="name">
+    </div>
+    <div>
+        <label for="email">Digite o e-mail do usuário</label>
+        <input type="email" name="email" id="email">
+    </div>
+    <div>
+        <label for="password">Digite uma senha para o usuário</label>
+        <input type="password" name="password" id="password">
+    </div>
+    <div>
+        <button type="submit">Cadastrar Usuário</button>
+    </div>
+
+</form>
