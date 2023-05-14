46f8d7f3a7c54d372cd2191a23086f384e3e82c0
LucasGFBatista <lucas.gf.batista@gmail.com>
2023-05-14 18:11:54 -0300

    ajustes de tela de cadastro de cliente

--- Semestre-2/Programacao-web/Atividades/salao-de-beleza/index.php
+++ Semestre-2/Programacao-web/Atividades/salao-de-beleza/index.php
@@ -4,35 +4,49 @@ include "./inc/header.php";
 
 ?>
 
-
+<div class="container py-2 insertTitle">
+    <h2>Cadastro de cliente</h2>
+</div>
 <form action="./config/insert.php" method="post">
 
-    <div class="container py-5">
+
+    <div class="container py-2">
+
 
         <div class="box-basicInfo">
-            <h2>Informações básicas</h2>
 
-            <div class="mb-3">
-                <label for="name">Digite o nome de cliente</label>
-                <input type="text" name="name" id="name" value="">
+            <div class="basicInfoTitle">
+                <h3>Informações básicas</h3>
             </div>
 
-            <div class="mb-3">
-                <label for="email">Digite o e-mail de cliente</label>
-                <input type="email" name="email" id="email" value="">
-            </div>
+            <div class="basicInfoForm">
+                <div class="mb-3">
+                    <label for="name">Nome de cliente</label>
+                    <input type="text" name="name" id="name" value="">
+                </div>
 
-            <div class="mb-3">
-                <label for="birthday_date">Digite a data de nascimento</label>
-                <input type="date" id="birthday_date" name="birthday_date" value="Today" min="1940-01-01" max="2023-12-01">
+                <div class="mb-3">
+                    <label for="email">E-mail de cliente</label>
+                    <input type="email" name="email" id="email" value="">
+                </div>
+
+                <div class="mb-3">
+                    <label for="birthday_date">Data de nascimento</label>
+                    <input type="date" id="birthday_date" name="birthday_date" value="Today" min="1940-01-01" max="2023-12-01">
+                </div>
+
             </div>
 
         </div>
-
-        <hr>
-
+    </div>
+    <hr>
+    <div class="container py-2">
         <div class="box-endereco">
-            <h2>Endereço</h2>
+
+            <div class="basicInfoTitle">
+                <h3>Endereço</h3>
+            </div>
+
 
             <div class="mb-3">
                 <label for="endereco">Endereço</label>
@@ -56,16 +70,14 @@ include "./inc/header.php";
                 <input type="text" name="cep" id="cep" value="">
             </div>
 
-
-
         </div>
 
 
-        <div>
+        <div class="container py-5">
             <button type="submit" class="btn btn-primary">Cadastrar cliente</button>
         </div>
-
     </div>
+
 </form>
 
 <?php
